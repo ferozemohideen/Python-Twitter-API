465ed02c13204948afe0534413090fd944aceb73
Feroze Mohideen <fm87@duke.edu>
2018-07-13 09:52:31 -0500

    initial commit

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'XvfOn2gQlMVGDBG2DYy4ZWlrT'
+CONSUMER_SECRET = 'CrYEGFTIOKuvPRL80HNCoHWWyDkHKFPrkRYkmOdq5PuPw0ETOE'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
